7f6305162677ea9a1eea8660c6446d1c088dfdb0
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-11 19:27:47 +0700

    add: aesthetics and treesitter

--- lua/ryuun1corn/core/keymaps.lua
+++ lua/ryuun1corn/core/keymaps.lua
@@ -22,6 +22,3 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
--- file explorer
-keymap.set("n", "<leader>pv", "<cmd>:Ex<CR>", { desc = "Return to file explorer" })
