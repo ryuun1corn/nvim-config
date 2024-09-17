8a94d2b5eb1eede11533cca449434fa55a48d6a5
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-17 10:18:39 +0700

    add and configure lsp

--- lua/ryuun1corn/lazy.lua
+++ lua/ryuun1corn/lazy.lua
@@ -15,7 +15,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("ryuun1corn.plugins", {
+require("lazy").setup({ { import = "ryuun1corn.plugins" }, { import = "ryuun1corn.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false
+  },
   change_detection = {
     notify = false,
   },
