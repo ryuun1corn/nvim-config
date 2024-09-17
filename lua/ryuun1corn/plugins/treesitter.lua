4f095de1b4dcd47697ee7ba7b31db55f094cf0dd
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-17 22:48:56 +0700

    formatting fix

--- lua/ryuun1corn/plugins/treesitter.lua
+++ lua/ryuun1corn/plugins/treesitter.lua
@@ -1,59 +1,64 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w /nvim-ts-autotag plugin)
-      autotag = {
-        enable = true
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "bash",
-        "c",
-        "cmake",
-        "cpp",
-        "css",
-        "dockerfile",
-        "gitignore",
-        "html",
-        "java",
-        "javascript",
-        "json",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w /nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"bash",
+				"c",
+				"cmake",
+				"cpp",
+				"css",
+				"dockerfile",
+				"gitignore",
+				"html",
+				"java",
+				"javascript",
+				"json",
+				"lua",
+				"make",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
 
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<space>hi",
-          node_incremental = "<space>hk",
-          scope_incremental = false,
-          node_decremental = "<space>hj",
-        },
-      },
-    })
-  end
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<space>hi",
+					node_incremental = "<space>hk",
+					scope_incremental = false,
+					node_decremental = "<space>hj",
+				},
+			},
+
+			sync_install = false,
+			ignore_install = {},
+			auto_install = true,
+			modules = {},
+		})
+	end,
 }
