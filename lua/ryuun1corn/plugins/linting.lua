15370b22b2da1d3b4b7b94d1607b3b3bf575d310
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-22 18:15:47 +0700

    fix: remove jinja lsp lint (unsupported in nvim lint)

--- lua/ryuun1corn/plugins/linting.lua
+++ lua/ryuun1corn/plugins/linting.lua
@@ -9,7 +9,7 @@ return {
 			typescript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
-			python = { "pylint", "jinja_lsp" },
+			python = { "pylint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
