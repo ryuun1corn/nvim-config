942493e921e809bab529b67c2bc64e957fa37855
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-10-02 15:51:17 +0700

    refactor: increase formatting timeout time to 5s

--- lua/ryuun1corn/plugins/formatting.lua
+++ lua/ryuun1corn/plugins/formatting.lua
@@ -23,7 +23,7 @@ return {
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 2500,
+				timeout_ms = 5000,
 			},
 		})
 
@@ -31,7 +31,7 @@ return {
 			conform.format({
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 5000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,
