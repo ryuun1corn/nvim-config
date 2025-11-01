d730968c04a0e019d30b20087ba1b89cc76ec233
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2025-11-02 00:08:24 +0700

    fix: nvim lsp config shits

--- lua/ryuun1corn/plugins/lsp/lspconfig.lua
+++ lua/ryuun1corn/plugins/lsp/lspconfig.lua
@@ -7,12 +7,6 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
-		local mason_lspconfig = require("mason-lspconfig")
-
 		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
@@ -71,94 +65,171 @@ return {
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		-- Apply cmp capabilities to ALL LSP clients by default (0.11+ feature)
+		vim.lsp.config("*", {
+			capabilities = capabilities,
+		})
+
+		-- Emmet (use the newer server name)
+		-- Docs + options: https://github.com/olrtg/emmet-language-server
+		vim.lsp.config("emmet_language_server", {
+			filetypes = {
+				"html",
+				"htmldjango",
+				"css",
+				"sass",
+				"scss",
+				"less",
+				"javascriptreact",
+				"typescriptreact",
+				"svelte",
+				"pug",
+				"eruby",
+			},
+			init_options = {
+				-- Emmet preferences (maps your old ["bem.enabled"] option)
+				preferences = { ["bem.enabled"] = true },
+				showExpandedAbbreviation = "always",
+			},
+		})
+
+		-- Lua (with neodev; keep your “vim” global + snippet behavior)
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = { globals = { "vim" } },
+					completion = { callSnippet = "Replace" },
+				},
+			},
+		})
+
+		-- Jinja (jinja-lsp)
+		-- Install via Mason (“jinja-lsp”) or `cargo install jinja-lsp`
+		vim.lsp.config("jinja_lsp", {
+			cmd = { "jinja-lsp" },
+			-- Attach on these filetypes; include Django templates if you want it there
+			filetypes = { "jinja", "htmldjango" },
+			-- Simple, fast root detection (0.11+ supports root_markers directly)
+			root_markers = { ".git", "pyproject.toml", "manage.py" },
+			settings = {
+				jinja = {
+					env = { django = true, customTags = {}, customFilters = {} },
+					trim_blocks = true,
+					lstrip_blocks = true,
+				},
+			},
+		})
+
+		-- HTML (extend to work on htmldjango too)
+		vim.lsp.config("html", {
+			filetypes = { "html", "htmldjango" },
+		})
+
+		-- If you use mason-lspconfig, prefer managing enablement yourself
+		local ok_mlsp, mlsp = pcall(require, "mason-lspconfig")
+		if ok_mlsp then
+			mlsp.setup({
+				ensure_installed = {
+					"lua_ls",
+					"html",
+					"emmet_language_server",
+					"jinja_lsp",
+				},
+				automatic_enable = false, -- we call vim.lsp.enable() below
+			})
 		end
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
+		-- Finally, enable the servers (0.11+ supports enabling a list)
+		vim.lsp.enable({ "emmet_language_server", "lua_ls", "jinja_lsp", "html" })
 
-			["emmet_ls"] = function()
-				-- configure emmet language server
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"htmldjango",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
-					},
-					init_options = {
-						html = {
-							options = {
-								-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-								["bem.enabled"] = true,
-							},
-						},
-					},
-				})
-			end,
+		-- Change the Diagnostic symbols in the sign column (gutter)
+		-- (not in youtube nvim video)
+		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		-- for type, icon in pairs(signs) do
+		-- 	local hl = "DiagnosticSign" .. type
+		-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		-- end
 
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
+		-- mason_lspconfig.setup_handlers({
+		-- 	-- default handler for installed servers
+		-- 	function(server_name)
+		-- 		lspconfig[server_name].setup({
+		-- 			capabilities = capabilities,
+		-- 		})
+		-- 	end,
 
-			["jinja_lsp"] = function()
-				lspconfig["jinja_lsp"].setup({
-					capabilities = capabilities,
-					cmd = { "jinja-lsp" },
-					filetypes = { "htmldjango" },
-					root_dir = function(fname)
-						return lspconfig.util.find_git_ancestor(fname)
-					end,
-					settings = {
-						jinja = {
-							env = {
-								django = true,
-								customTags = {},
-								customFilters = {},
-							},
-							trim_blocks = true,
-							lstrip_blocks = true,
-						},
-					},
-				})
-			end,
+		-- 	["emmet_ls"] = function()
+		-- 		-- configure emmet language server
+		-- 		lspconfig["emmet_ls"].setup({
+		-- 			capabilities = capabilities,
+		-- 			filetypes = {
+		-- 				"html",
+		-- 				"htmldjango",
+		-- 				"typescriptreact",
+		-- 				"javascriptreact",
+		-- 				"css",
+		-- 				"sass",
+		-- 				"scss",
+		-- 				"less",
+		-- 				"svelte",
+		-- 			},
+		-- 			init_options = {
+		-- 				html = {
+		-- 					options = {
+		-- 						-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+		-- 						["bem.enabled"] = true,
+		-- 					},
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
 
-			["html"] = function()
-				lspconfig["html"].setup({
-					capabilities = capabilities,
-					filetypes = { "html", "htmldjango" },
-				})
-			end,
-		})
+		-- 	["lua_ls"] = function()
+		-- 		-- configure lua server (with special settings)
+		-- 		lspconfig["lua_ls"].setup({
+		-- 			capabilities = capabilities,
+		-- 			settings = {
+		-- 				Lua = {
+		-- 					-- make the language server recognize "vim" global
+		-- 					diagnostics = {
+		-- 						globals = { "vim" },
+		-- 					},
+		-- 					completion = {
+		-- 						callSnippet = "Replace",
+		-- 					},
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
+
+		-- 	["jinja_lsp"] = function()
+		-- 		lspconfig["jinja_lsp"].setup({
+		-- 			capabilities = capabilities,
+		-- 			cmd = { "jinja-lsp" },
+		-- 			filetypes = { "htmldjango" },
+		-- 			root_dir = function(fname)
+		-- 				return lspconfig.util.find_git_ancestor(fname)
+		-- 			end,
+		-- 			settings = {
+		-- 				jinja = {
+		-- 					env = {
+		-- 						django = true,
+		-- 						customTags = {},
+		-- 						customFilters = {},
+		-- 					},
+		-- 					trim_blocks = true,
+		-- 					lstrip_blocks = true,
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
+
+		-- 	["html"] = function()
+		-- 		lspconfig["html"].setup({
+		-- 			capabilities = capabilities,
+		-- 			filetypes = { "html", "htmldjango" },
+		-- 		})
+		-- 	end,
+		-- })
 	end,
 }
